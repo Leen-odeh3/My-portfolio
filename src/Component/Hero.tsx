d043c06c2d3fcd1000b6fe57c8a839d83c952e3d
leen <leenodeh287@gmail.com>
2023-12-11 22:09:24 +0300

    done HeroSection

--- src/Component/Hero.tsx
+++ src/Component/Hero.tsx
@@ -1,6 +1,7 @@
 import { Box, Grid, Stack, Typography } from "@mui/material";
 import { Social } from "../data/Social";
 import RightSide from "./RightSide";
+import Buttons from "./Buttons";
 
 const Hero = () => {
   return (
@@ -9,7 +10,7 @@ const Hero = () => {
         flexGrow: 1,
         margin: { xs: "10px 20px", md: "10px 30px" },
         padding: { xs: "10px 20px", md: "20px 40px" },
-        alignItems: 'center'
+        alignItems: "center",
       }}
     >
       <Grid container spacing={3}>
@@ -17,12 +18,17 @@ const Hero = () => {
           <Box className="info">
             <Typography
               component="h1"
-              sx={{ fontSize: "40px", marginBottom: "20px", color: "#001c55",paddingTop: { md: "70px" } }}
+              sx={{
+                fontSize: "40px",
+                marginBottom: "20px",
+                color: "#001c55",
+                paddingTop: { md: "10px" },
+              }}
             >
-              Leen Odeh
+              Hi all, I'm Leen Odeh
             </Typography>
             <Typography component="h2" sx={{ marginBottom: "20px" }}>
-              Full Stack Developer⌛️
+              Full Stack Developer ⌛️
             </Typography>
             <Typography
               component="p"
@@ -30,7 +36,7 @@ const Hero = () => {
                 color: "rgb(127, 141, 170)",
                 fontSize: { xs: "20px" },
                 lineHeight: "40px",
-                margin: { md: "20px 40px 20px 0px", xs: "20px auto 20px" },
+                margin: { md: "20px 40px 15px 0px", xs: "20px auto 20px" },
               }}
             >
               A passionate individual who always thrives to work on end-to-end
@@ -39,28 +45,49 @@ const Hero = () => {
             </Typography>
           </Box>
           <Box>
-            <Stack
-              direction="row"
-              spacing={3}
-        
-            >
+            <Stack direction="row" spacing={3} mb={3}>
               {Social.map((socialLink, index) => (
                 <a
                   key={index}
                   href={socialLink.url}
                   target="-blank"
                   className={`${socialLink.name} links`}
-                  style={{margin:"auto",marginBottom:"20px"}}
+                  style={{ margin: "auto", marginBottom: "20px" }}
                 >
                   {socialLink.icons}
                 </a>
               ))}
             </Stack>
           </Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              paddingRight: "30px",
+            }}
+          >
+            <Buttons
+              content={
+                <>
+                  <i
+                    className="bi bi-star-fill"
+                    style={{
+                      marginRight: "10px",
+                      color: "rgb(254, 171, 17)",
+                      fontSize: "20px",
+                    }}
+                  ></i>
+                <span className="link-btn"> Star Me On Github</span>  
+                </>
+              }
+              url="https://github.com/Leen-odeh3/Portfolio"
+            />
+          </Box>
         </Grid>
 
         <Grid item xs={12} md={6}>
-          <RightSide/>
+          <RightSide />
         </Grid>
       </Grid>
     </Box>
