b3582a9579d497869e338371f6b0fd8a49c19bef
leen <leenodeh287@gmail.com>
2023-12-15 18:01:00 +0300

    Update HeroSection

--- src/Component/Hero/Hero.tsx
+++ src/Component/Hero/Hero.tsx
@@ -13,7 +13,7 @@ const Hero = () => {
         alignItems: "center",
       }}
     >
-      <Grid container spacing={3}>
+      <Grid container spacing={5}>
         <Grid item xs={12} md={6}>
           <Box className="info">
             <Typography
--- src/Component/RightSide/RightSide.tsx
+++ src/Component/RightSide/RightSide.tsx
@@ -1,9 +1,27 @@
+import { Box } from "@mui/material"
+import './RightSide.scss'
 
 const RightSide = () => {
   return (
-    <div style={{padding:"20px"}}>
-      <img src="../../public/HeroImg.gif" alt="hero-img" width="90%" height="100%"
-       style={{objectFit:"cover"}}/>
+    <Box className="parent" sx={{backgroundColor:"rgb(109, 131, 242)",padding:{xs:"30px",md:"30px"}}}>
+      <Box className="child" sx={{borderRadius:"10px",padding:{xs:"10px",md:"20px"},
+      backgroundColor:"rgb(248, 248, 248)",boxShadow:"rgba(0, 0, 0, 0.05) 0px 5px 10px",color:"rgb(109, 131, 242)"}}>
+      <pre>
+        1&nbsp;&nbsp;class <b>Person</b> {'{'}
+      </pre>
+      <pre>2&nbsp;&nbsp;&nbsp; constructor() {'{'}</pre>
+      <pre>
+        3&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;this.name = "<b>Leen Odeh</b>";
+      </pre>
+      <pre>
+        4&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;this.traits = ["<b>DESIGN</b>", "<b>DEV</b>"];
+      </pre>
+      <pre>5&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;this.age = new Date().getFullYear() - 2001;</pre>
+      <pre>6&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {'}'}</pre>
+      <pre>7&nbsp;&nbsp;{'}'}</pre>
+      </Box>
+      {/* <img src="../../public/HeroImg.gif" alt="hero-img" width="90%" height="100%"
+       style={{objectFit:"cover"}}/> */}
 
         <style>
       {
@@ -17,7 +35,7 @@ const RightSide = () => {
         `
       }
     </style> 
-    </div>
+    </Box>
 
   
   )
